c9e200f3c257dea0f78b1ecc2deef14caa4f2fc0
Saif <seifeddinerejeb.sr@gmail.com>
2025-01-21 00:18:38 +0100

    Task 6: Test Automation

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# activate the virtual environment
+. ./venv/bin/activate
+
+# run the test suite
+python -m pytest test_pink_morsel_visualizer.py
+
+# exit code is 0 if all tests pass
+PYTEST_EXIT_CODE=$?
+
+# return exit code 0 if all tests pass or 1 otherwise
+if [ $PYTEST_EXIT_CODE -eq 0 ]
+then
+  exit 0
+else
+  exit 1
+fi
